50598a8c4566314e237416ab1c30e0f2fa1ebc04
Joël Stemmer <stemmertech@gmail.com>
2018-07-21 20:58:40 +0100

    Use pgx.ConnPool instead of pgx.Conn

--- pgxdb/pgxdb.go
+++ pgxdb/pgxdb.go
@@ -12,18 +12,18 @@ import (
 // Database implements the sqlstore.Database interface for a PostgreSQL
 // database.
 type Database struct {
-	conn *pgx.Conn
+	pool *pgx.ConnPool
 }
 
 // New creates a new pgxdb.Database instance with the given pgx connection
 // conn.
-func New(conn *pgx.Conn) *Database {
-	return &Database{conn}
+func New(pool *pgx.ConnPool) *Database {
+	return &Database{pool}
 }
 
 // Load loads the session identified by id from the database.
 func (d *Database) Load(id string) (updatedAt time.Time, data []byte, err error) {
-	row := d.conn.QueryRow("SELECT data, updated_at FROM sessions WHERE id = $1 LIMIT 1", id)
+	row := d.pool.QueryRow("SELECT data, updated_at FROM sessions WHERE id = $1 LIMIT 1", id)
 	if err = row.Scan(&data, &updatedAt); err != nil {
 		if err == sql.ErrNoRows {
 			return time.Time{}, nil, nil
@@ -35,18 +35,18 @@ func (d *Database) Load(id string) (updatedAt time.Time, data []byte, err error)
 
 // Insert saves a new session to the database.
 func (d *Database) Insert(id string, data []byte) error {
-	_, err := d.conn.Exec("INSERT INTO sessions(id, data, created_at, updated_at) VALUES ($1, $2, NOW(), NOW())", id, data)
+	_, err := d.pool.Exec("INSERT INTO sessions(id, data, created_at, updated_at) VALUES ($1, $2, NOW(), NOW())", id, data)
 	return err
 }
 
 // Update updates an existing session in the database.
 func (d *Database) Update(id string, data []byte) error {
-	_, err := d.conn.Exec("UPDATE sessions SET data=$2, updated_at=NOW() WHERE id=$1", id, data)
+	_, err := d.pool.Exec("UPDATE sessions SET data=$2, updated_at=NOW() WHERE id=$1", id, data)
 	return err
 }
 
 // Delete deletes the session identified by id from the database.
 func (d *Database) Delete(id string) error {
-	_, err := d.conn.Exec("DELETE FROM sessions WHERE ID=$1", id)
+	_, err := d.pool.Exec("DELETE FROM sessions WHERE ID=$1", id)
 	return err
 }
