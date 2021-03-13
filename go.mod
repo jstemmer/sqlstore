8916d0965a147fe4fb707f2211f929231aec8f5d
Joël Stemmer <stemmertech@gmail.com>
2021-03-13 13:44:56 +0000

    Upgrade to github.com/jackc/pgx/v4

--- go.mod
+++ go.mod
@@ -3,13 +3,11 @@ module github.com/jstemmer/sqlstore
 go 1.16
 
 require (
-	github.com/cockroachdb/apd v1.1.0 // indirect
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/google/go-cmp v0.5.5
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/sessions v1.2.1
-	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
-	github.com/jackc/pgx v3.6.2+incompatible
+	github.com/jackc/pgx/v4 v4.10.1
 	github.com/lib/pq v1.10.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
--- pgxdb/pgxdb.go
+++ pgxdb/pgxdb.go
@@ -7,24 +7,24 @@ import (
 	"database/sql"
 	"time"
 
-	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 // Database implements the sqlstore.Database interface for a PostgreSQL
 // database.
 type Database struct {
-	pool *pgx.ConnPool
+	pool *pgxpool.Pool
 }
 
 // New creates a new pgxdb.Database instance with the given pgx connection
 // conn.
-func New(pool *pgx.ConnPool) *Database {
+func New(pool *pgxpool.Pool) *Database {
 	return &Database{pool}
 }
 
 // Load loads the session identified by id from the database.
 func (d *Database) Load(ctx context.Context, id string) (updatedAt time.Time, data []byte, err error) {
-	row := d.pool.QueryRowEx(ctx, "SELECT data, updated_at FROM sessions WHERE id = $1 LIMIT 1", nil, id)
+	row := d.pool.QueryRow(ctx, "SELECT data, updated_at FROM sessions WHERE id = $1 LIMIT 1", id)
 	if err = row.Scan(&data, &updatedAt); err != nil {
 		if err == sql.ErrNoRows {
 			return time.Time{}, nil, nil
@@ -36,18 +36,18 @@ func (d *Database) Load(ctx context.Context, id string) (updatedAt time.Time, da
 
 // Insert saves a new session to the database.
 func (d *Database) Insert(ctx context.Context, id string, data []byte) error {
-	_, err := d.pool.ExecEx(ctx, "INSERT INTO sessions(id, data, created_at, updated_at) VALUES ($1, $2, NOW(), NOW())", nil, id, data)
+	_, err := d.pool.Exec(ctx, "INSERT INTO sessions(id, data, created_at, updated_at) VALUES ($1, $2, NOW(), NOW())", id, data)
 	return err
 }
 
 // Update updates an existing session in the database.
 func (d *Database) Update(ctx context.Context, id string, data []byte) error {
-	_, err := d.pool.ExecEx(ctx, "UPDATE sessions SET data=$2, updated_at=NOW() WHERE id=$1", nil, id, data)
+	_, err := d.pool.Exec(ctx, "UPDATE sessions SET data=$2, updated_at=NOW() WHERE id=$1", id, data)
 	return err
 }
 
 // Delete deletes the session identified by id from the database.
 func (d *Database) Delete(ctx context.Context, id string) error {
-	_, err := d.pool.ExecEx(ctx, "DELETE FROM sessions WHERE ID=$1", nil, id)
+	_, err := d.pool.Exec(ctx, "DELETE FROM sessions WHERE ID=$1", id)
 	return err
 }
